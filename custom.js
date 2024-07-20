366f6d171b59d9ef0ca686e64034b214ae94a1b5
hitumabushi <138351825+hi2ma-bu4@users.noreply.github.com>
2024-07-20 21:45:20 +0900

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,8 +1,8 @@
-(function() {
+setTimeout(() => {
   console.log("custom-js Loaded!");
-  const isWorkspace = !vscode.context.configuration().workspace.uri;
+  const isWorkspace = !vscode.context.configuration()?.workspace?.uri;
   console.log("custom-js isWorkspace:",isWorkspace);
   if(!isWorkspace){
     document.body.classList.add("custom-isWorkspace");
   }
-})();
+}, 1000);
